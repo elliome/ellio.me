ad229a20126a64fc4ccf49a525b02afbe41ad606
Elliot Emmerson <elliotluke.emmerson@gmail.com>
2021-06-21 20:57:42 +0100

    now playing in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,50 @@
+import Link from "next/link";
+import useFetcher from "../hooks/DataFetching";
 import styles from "../styles/components/Footer.module.scss";
+import Image from "./Image";
 
 const Footer = () => {
+    const { data: currentlyPlaying, mutate: mutateCurrentlyPlaying } =
+        useFetcher("/api/spotify/playing");
+
     return (
         <div className={styles.container}>
-            <div className={styles.section}>Spotify</div>
+            {currentlyPlaying?.item?.name ? (
+                <Link href={"/listening"}>
+                    <a className={`${styles.nowPlaying} ${styles.section}`}>
+                        {currentlyPlaying?.is_playing ? (
+                            <div
+                                className={`${styles.nowPlaying} ${styles.section}`}>
+                                <div
+                                    className={styles.nowPlayingImageContainer}>
+                                    <Image
+                                        src={
+                                            currentlyPlaying?.item?.album
+                                                ?.images?.[2]?.url
+                                        }
+                                        height={20}
+                                        width={20}
+                                        placeholder="blur"
+                                        blurDataURL={currentlyPlaying.base64}
+                                        key={
+                                            currentlyPlaying?.item?.album
+                                                ?.images?.[2]?.url
+                                        }
+                                        alt=""
+                                    />
+                                </div>
+                                <span>{currentlyPlaying?.item?.name}</span>
+                            </div>
+                        ) : (
+                            <div className={styles.section}>
+                                Paused: {currentlyPlaying?.item?.name}
+                            </div>
+                        )}
+                    </a>
+                </Link>
+            ) : (
+                <div className={styles.section}>Music</div>
+            )}
             <div className={styles.section}>Built with</div>
             <div className={`${styles.section} ${styles.socials}`}>
                 <div></div>
